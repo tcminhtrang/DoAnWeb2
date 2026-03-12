10e441cdd0a57f2abc0e30923d2b42aaef780d39
tcminhtrang <tc.minhtrangg@gmail.com>
2026-03-12 07:51:02 +0700

    Initial commit cho DoAnWeb2

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+const userInput = document.getElementById("username");
+const passInput = document.getElementById("password");
+const error = document.getElementById("errorMsg");
+
+[userInput, passInput].forEach(input => {
+  input.addEventListener("input", () => error.textContent = "");
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = userInput.value.trim();
+  const password = passInput.value.trim();
+
+  error.textContent = "";
+
+  if (!username || !password) {
+    error.textContent = "Vui lòng nhập đầy đủ thông tin.";
+    return;
+  }
+
+  const validUser = "Admin123@gmail.com";
+  const validPass = "Admin@2025";
+
+  if (username === validUser && password === validPass) {
+    window.location.href = "admin/dashboard.html";
+  } else {
+    error.textContent = "Sai tài khoản hoặc mật khẩu.";
+  }
+});
